821b8bf9396c33bd28e48a20aeeb7db8b55f6427
Ronald Lamoreaux <gitkey@chindraba.work>
2023-07-16 23:28:19 +0000

    Extract message output functions
    Signed-off-by: Ronald Lamoreaux <gitkey@chindraba.work>

---
+++ scripts/errorOut.sh
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: MIT
+
+# Copyright 2023 Chindraba (Ronald Lamoreaux) <DishUSBImager@chindraba.work>
+
+# Messages file for DishUSBImager script
+
+errorOut() { printf "%s\n" "${@}" >&2; }
+
+# vim: syntax=bash ts=4 sw=4 sts=4 et sr
---
+++ scripts/messageOut.sh
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: MIT
+
+# Copyright 2023 Chindraba (Ronald Lamoreaux) <DishUSBImager@chindraba.work>
+
+# Messages file for DishUSBImager script
+
+messageOut() { printf "%s\n" "${@}"; }
+
+# vim: syntax=bash ts=4 sw=4 sts=4 et sr
